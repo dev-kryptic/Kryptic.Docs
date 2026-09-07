95c34868735a7db3db369e939ee07459c89fedcd
Theodosis Katsoulis <theodosis.kats@gmail.com>
2026-09-07 10:39:40 +0300

    Add Open Graph tags so shared docs links show the Kryptic logo.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -13,6 +13,11 @@ import 'react-toastify/dist/ReactToastify.css'
 import { dom } from '@fortawesome/fontawesome-svg-core'
 import { ImageZoom } from '@/components/ImageZoom'
 
+const DOCS_ORIGIN = 'https://docs.kryptic.dev'
+const SHARE_IMAGE = `${DOCS_ORIGIN}/logo.png`
+const DEFAULT_DESCRIPTION =
+  'Documentation for Kryptic - secrets management for developers.'
+
 function onRouteChange() {
   useMobileNavigationStore.getState().close()
 }
@@ -20,16 +25,43 @@ function onRouteChange() {
 Router.events.on('routeChangeStart', onRouteChange)
 Router.events.on('hashChangeStart', onRouteChange)
 
+function canonicalPath(asPath) {
+  const path = (asPath ?? '/').split('#')[0].split('?')[0]
+  return path === '' ? '/' : path
+}
+
 export default function App({ Component, pageProps }) {
   let router = useRouter()
   let tableOfContents = collectHeadings(pageProps.sections)
+  const title = `${pageProps.title} - Kryptic Docs`
+  const description = pageProps.description || DEFAULT_DESCRIPTION
+  const canonical = `${DOCS_ORIGIN}${canonicalPath(router.asPath)}`
 
   return (
     <>
       <Head>
         <style>{dom.css()}</style>
-        <title>{`${pageProps.title} - Kryptic Docs`}</title>
-        <meta name="description" content={pageProps.description} />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="canonical" href={canonical} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Kryptic Docs" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:url" content={canonical} />
+        <meta property="og:image" content={SHARE_IMAGE} />
+        <meta property="og:image:secure_url" content={SHARE_IMAGE} />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1000" />
+        <meta property="og:image:height" content="1000" />
+        <meta property="og:image:alt" content="Kryptic" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={SHARE_IMAGE} />
+        <meta name="twitter:image:alt" content="Kryptic" />
+        <link rel="image_src" href={SHARE_IMAGE} />
       </Head>
       <MDXProvider components={mdxComponents}>
         <Layout
